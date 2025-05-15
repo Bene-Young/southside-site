e238673c2f1e13180f5b04bb99ebddad42d1948b
Bene-Young <131826983+Bene-Young@users.noreply.github.com>
2025-05-15 23:43:59 +0200

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "2rem", fontFamily: "Arial" }}>
+      <h1>Bienvenue sur Southside RP</h1>
+      <p>Le site est bien en ligne !</p>
+    </main>
+  );
+}
